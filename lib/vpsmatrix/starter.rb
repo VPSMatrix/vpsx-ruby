3eae9c5bef7ba4a281fee24b7ae67111f5980fbd
mousse <mousse@timepress.cz>
2016-06-03 21:41:48 +0200

    Scattered working code for file uploads and API calls

--- lib/vpsmatrix/starter.rb
+++ lib/vpsmatrix/starter.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'digest'
 
 class Starter < Thor
   include Thor::Actions
@@ -20,6 +21,39 @@ class Starter < Thor
       # read API KEY
 
 
+    working_dir = Dir.pwd
+    list_of_files = Dir.glob "#{working_dir}/**/*"
+    files_string = ""
+
+    # TODO deal with images
+    list_of_files.map do |file|
+      files_string += "#{file}\n"
+      files_string += File.read(file)
+    end
+
+
+    uri = URI("https://api.vpsmatrix.net/uploads/send_new_files")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+
+    request.set_form_data({"file" => "string"})
+    response = http.request(request)
+
+
+    require "net/http"
+    require "uri"
+
+    # get files
+    uri = URI.parse("https://api.vpsmatrix.net/uploads/get_file_list")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri.path)
+    req.basic_auth("test_app", "test_app")
+    http.request(req)
+
+
+    # https://api.vpsmatrix.net/uploads/get_new_files
 
     # detect rails? DB in (pg, mysql, sqlite, nodb)?
     # no? - do you wish us to check it?
@@ -36,6 +70,73 @@ class Starter < Thor
     # receive DONE deploy -> show URL
 
   end
+
+  no_commands do
+    def read_files
+
+      # TODO check size of directory
+      working_dir = Dir.pwd
+      list_of_files = Dir.glob "#{working_dir}/**/*"
+      list_of_files.reject! {|path| path =~ /\/log|\/tmp/}
+      File.open("files_to_send", 'w') do |temp_file|
+        list_of_files.each do |file|
+          if File.file? file
+            file_content = File.read(file)
+            temp_file.write "#{file}\n"
+            temp_file.write "#{file_content}\n"
+            temp_file.write "#{Digest::SHA256.hexdigest(file_content)}\n"
+          end
+        end
+      end
+
+      uri = URI.parse("https://api.vpsmatrix.net/uploads/send_new_files")
+
+      # stream version
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        req = Net::HTTP::Put.new(uri)
+        req.add_field('Transfer-Encoding', 'chunked')
+        request.basic_auth("test_app", "test_app")
+        req.body_stream = File.open("files_to_send")
+        #http.request(req)
+
+        #request = Net::HTTP::Put.new(uri.request_uri)
+        #request.set_form_data({"file" => File.read("files_to_send")})
+        http.request request do |response|
+         # puts response
+          response.read_body do |chunk|
+            puts chunk
+          end
+        end
+      end
+
+      # no stream version
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        request = Net::HTTP::Put.new(uri.request_uri)
+        request.basic_auth("test_app", "test_app")
+        request.set_form_data({"file" => File.read("files_to_send")})
+        http.request request #do |response|
+        #puts response
+        #response.read_body do |chunk|
+        #  puts chunk
+        #end
+        #end
+      end
+    end
+
+    def read_dirs
+      working_dir = Dir.pwd
+      list_of_files = Dir.glob "#{working_dir}/**/*"
+      dirs_string = ""
+
+      list_of_files.map do |file|
+        if File.directory file
+          dirs_string += "#{file}\n"
+        end
+      end
+      dirs_string
+    end
+  end
+
 
 
 end
